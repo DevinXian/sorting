2091d118573898f2a39f2cd8d2fe04ffe0e5e474
luofeng <devinxian@gmail.com>
2020-09-01 02:28:37 +0800

    shell sort & insert sort common feature

--- src/insert.js
+++ src/insert.js
@@ -13,7 +13,7 @@ module.exports = function insertSort(arr) {
   const len = arr.length
 
   // 当前有序列表[0, i-1] 
-  for (let i = 1; i <= len - 1; i++) {
+  for (let i = 1; i < len; i++) {
     let ele = arr[i] // 变量单独赋值，不然会被后续循环污染
     let j = i - 1;
 
@@ -29,6 +29,21 @@ module.exports = function insertSort(arr) {
     }
     arr[j] = ele
   }
+
+  /* 以下方法是将待插入元素加入位置变换，每次都移动目标元素，类似 shell.js  */
+  // // 当前有序列表[0, i-1] 
+  // for (let i = 1; i < len; i++) {
+  //   // 取下标i, 从后向前在有序列表查找合适位置-稳定
+  //   for (let j = i - 1; j >= 0; j--) {
+  //     // 如果当前位置比ele大，则挨个后移,给ele挪位置
+  //     if (arr[j + 1] < arr[j]) { // 此处条件若是 <= 则排序不稳定，原因：相等还向后让出空间，则后面的跑到前面插入了
+  //       swap(arr, j, j + 1)
+  //     } else {
+  //       // 当前位置元素 <= ele，符合条件，停止循环 - 稳定排序的原因
+  //       break;
+  //     }
+  //   }
+  // }
 
   return arr
 }
--- src/shell.js
+++ src/shell.js
@@ -11,25 +11,25 @@ const { swap } = require('../utils')
  */
 module.exports = function shellSort(arr) {
   const len = arr.length
-  let ele, j
-  let step = Math.floor(len / 2)
+  // let step = Math.floor(len / 2)
+  let step = len
 
   // 划分子序列，两个元素相隔 step
   while(step > 0) {
-    // 插入排序： 插入有序队列，故从子序列第二个元素开始(0 + step)
-    // 也可使用任意排序方法排序子序列，如冒泡等。
+    // 一次跳step，参考插入排序，移动元素到合适位置
+    // 两种方法： 1. 将当前元素暂存，在有序序列中找到合适位置插入（参考insert.js）
+    //          2. 将当前元素加入移动，比较中不断变换位置，类似冒泡（本代码实现）
     for (let i = step; i < len; i += 1) {
-      ele = arr[i]
       // 子序列循环，从后面向前搜索合适位置插入目标元素
-      for (j = i - step; j >= 0; j -= step) {
-        if (arr[j] > ele) {
+      for (let j = i - step; j >= 0; j -= step) {
+        if (arr[j] > arr[j + step]) {
           swap(arr, j, j + step) // 子序列中向后移动
         } else {
           break;
         }
       }
-      arr[j + step] = ele
     }
+
     step = Math.floor(step / 2);
   }
 
